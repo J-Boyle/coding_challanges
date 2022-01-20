9191de6b26a2bdad8f6ad8f4990e187e31805b78
James Boyle <52674661+J-Boyle@users.noreply.github.com>
2022-01-20 22:16:00 +0000

    removed class structure, changed to functions. STILL can not get backtracking algorithm to work.

--- sudoku_challange/sudoku_csp.py
+++ sudoku_challange/sudoku_csp.py
@@ -1,95 +1,105 @@
-from csp import Constaint
-from typing import List, Dict, Tuple, TypeVar
+from typing import List, Tuple
 from math import sqrt
-from itertools import product, permutations, chain
-
-sudoku_puzzle = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
-
-V = TypeVar('V') # variable type
-D = TypeVar('D') # domain type
+from itertools import product
 
 
-
-class SudokuUtility:
-
-    def __init__(self,
-                 puzzle: List[List[int]]):
-        self.puzzle = puzzle
-        self.possible_numbers = [i for i in range(1,10)]
-
-    def get_columns(self, guess) -> List[List[int]]:
-        return [[row[i] for row in guess] for i in range(len(guess))]
-
-    def get_box_coordinates(self) -> List[Tuple[int, int]]:
-        """
-        coordinates are for top left corner of each "box" with 0,0 being on the top left corner
-        """
-        coords_numbers = [i for i in range(0, len(self.puzzle), int(sqrt(len(self.puzzle))))]
-        return list(product(coords_numbers, coords_numbers[::-1]))
-
-    def get_box(self, grid_coords: Tuple[int, int], guess) -> List[List[int]]:
-        number_of_rows = int(sqrt(len(self.puzzle)))
-        x1_coord = grid_coords[0]
-        x2_coord = x1_coord + number_of_rows
-        y_coord = grid_coords[1]
-
-        result = [guess[y][x1_coord:x2_coord] for y in [i for i in range(y_coord + number_of_rows)][-number_of_rows:]]
-
-        return [item for sublist in result for item in sublist]
-
-    def get_boxes(self, guess) -> List[List[int]]:
-        return [self.get_box(coord, guess) for coord in self.get_box_coordinates()]
-
-    def guess_is_correct(self, guess: List[List[int]]) -> bool:
-        cols = self.get_columns(guess)
-        boxes = self.get_boxes(guess)
-
-        return all([self.is_satisfied(guess), self.is_satisfied(cols), self.is_satisfied(boxes)])
-
-    def is_satisfied(self, variables: List[List[int]]) -> bool:
-
-        result = [[number for number in row if number!=0] for row in variables]
-
-        return all([len(set(attempt)) == len(attempt) for attempt in result])
-
-    def solve(self, puzzle: List[List[int]] = None):
-        local_puzzle = puzzle.copy()
-        for index_row, row in enumerate(local_puzzle):
-            for index_number, number in enumerate(local_puzzle):
-                if local_puzzle[index_row][index_number] == 0:
-                    for guess in self.possible_numbers:
-                        local_puzzle[index_row][index_number] = guess
-                        if self.guess_is_correct(local_puzzle):
-                            print('guess is correct')
-                            result = self.solve(local_puzzle)
-                            self.print_result(local_puzzle)
-                            if result is not None:
-                                return result
-                        else:
-                            print('guess is incorrect')
-        return None
+def get_columns(guess) -> List[List[int]]:
+    return [[row[i] for row in guess] for i in range(len(guess))]
 
 
-    def print_result(self, result: List[List[int]]) -> None:
-        print(*[row for row in result], sep='\n')
-        print('------')
+def get_box_coordinates(puzzle) -> List[Tuple[int, int]]:
+    """
+    coordinates are for top left corner of each "box" with 0,0 being on the top left corner
+    """
+    coords_numbers = [i for i in range(0, len(puzzle), int(sqrt(len(puzzle))))]
+    return list(product(coords_numbers, coords_numbers[::-1]))
+
+
+def get_box(grid_coords: Tuple[int, int], guess) -> List[List[int]]:
+    number_of_rows = int(sqrt(len(guess)))
+    x1_coord = grid_coords[0]
+    x2_coord = x1_coord + number_of_rows
+    y_coord = grid_coords[1]
+
+    result = [guess[y][x1_coord:x2_coord] for y in [i for i in range(y_coord + number_of_rows)][-number_of_rows:]]
+
+    return [item for sublist in result for item in sublist]
+
+
+def get_all_boxes(guess) -> List[List[int]]:
+    return [get_box(coord, guess) for coord in get_box_coordinates(guess)]
+
+
+def guess_is_correct(guess: List[List[int]]) -> bool:
+    cols = get_columns(guess)
+    boxes = get_all_boxes(guess)
+
+    return all([is_satisfied(guess), is_satisfied(cols), is_satisfied(boxes)])
+
+
+def is_satisfied(target: List[List[int]]) -> bool:
+    # target = row, col or box
+    # remove 0's to stop produces false results when comparing set lengths
+    result = [[number for number in row if number != 0] for row in target]
+
+    return all([len(set(attempt)) == len(attempt) for attempt in result])
+
+
+def find_empty(puzzle) -> Tuple[int, int]:
+    for index_row, row in enumerate(puzzle):
+        for index_number, number in enumerate(puzzle):
+            if puzzle[index_row][index_number] == 0:
+                return index_row, index_number
+
+    return None
+
+
+def solve(puzzle)-> bool:
+    """Back tracking algorithm"""
+
+    # Base case of recursion
+    empty_position = find_empty(puzzle)
+    print(empty_position)
+    if not empty_position:
+        # If there are no more empty positions puzzle is solved
+        return True
+    else:
+        row, col = empty_position
+
+    for guess in range(1, 10):  # todo make this dynamic
+        puzzle[row][col] = guess
+        if guess_is_correct(puzzle):
+            print('correct guess')
+            print(puzzle)
+            if solve(puzzle):
+                return True
+
+            puzzle[row][col] = 0
+            print('backtracking...')
+
+    return False
+
+
+def print_result(result: List[List[int]]) -> None:
+    print(*[row for row in result], sep='\n')
+    print('------')
 
 
 if __name__ == '__main__':
-    test = SudokuUtility(sudoku_puzzle)
-    x = test.solve(sudoku_puzzle)
-    if x is None:
-        print('No Solution Found')
-    else:
-        x.print_result()
+    sudoku_puzzle = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
 
+    if solve(sudoku_puzzle):
+        print_result(sudoku_puzzle)
+    else:
+        print('No Solution Found')
+        print_result(sudoku_puzzle)
--- sudoku_challange/tests.py
+++ sudoku_challange/tests.py
@@ -1,11 +1,26 @@
 import pytest
-from sudoku_challange.sudoku import SudokuSolver
+from sudoku_challange.sudoku_csp import get_columns, get_box_coordinates, get_box, get_all_boxes, guess_is_correct, \
+    is_satisfied, find_empty
 
 
 @pytest.fixture
 def question():
     return [
         [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+@pytest.fixture
+def wrong_answer():
+    return [
+        [5, 3, 7, 0, 7, 0, 0, 0, 0],
         [6, 0, 0, 1, 9, 5, 0, 0, 0],
         [0, 9, 8, 0, 0, 0, 0, 6, 0],
         [8, 0, 0, 0, 6, 0, 0, 0, 3],
@@ -32,46 +47,53 @@ def answer():
     ]
 
 
-@pytest.fixture
-def solver_with_question(question):
-    return SudokuSolver(question)
+def test_get_columns(question):
+    answer = [[5, 6, 0, 8, 4, 7, 0, 0, 0],
+              [3, 0, 9, 0, 0, 0, 6, 0, 0],
+              [0, 0, 8, 0, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 8, 0, 0, 4, 0],
+              [7, 9, 0, 6, 0, 2, 0, 1, 8],
+              [0, 5, 0, 0, 3, 0, 0, 9, 0],
+              [0, 0, 0, 0, 0, 0, 2, 0, 0],
+              [0, 0, 6, 0, 0, 0, 8, 0, 7],
+              [0, 0, 0, 3, 1, 6, 0, 5, 9]]
+    assert get_columns(question) == answer
+
+def test_get_box_coordinates(question):
+    assert get_box_coordinates(question) == [(0, 6), (0, 3), (0, 0), (3, 6), (3, 3), (3, 0), (6, 6), (6, 3), (6, 0)]
+
+def test_get_box(question):
+    assert get_box((3,3), question) == [0, 6, 0, 8, 0, 3, 0, 2, 0]
+
+def test_get_all_boxes(question):
+    answer = [[0, 6, 0, 0, 0, 0, 0, 0, 0],
+              [8, 0, 0, 4, 0, 0, 7, 0, 0],
+              [5, 3, 0, 6, 0, 0, 0, 9, 8],
+              [0, 0, 0, 4, 1, 9, 0, 8, 0],
+              [0, 6, 0, 8, 0, 3, 0, 2, 0],
+              [0, 7, 0, 1, 9, 5, 0, 0, 0],
+              [2, 8, 0, 0, 0, 5, 0, 7, 9],
+              [0, 0, 3, 0, 0, 1, 0, 0, 6],
+              [0, 0, 0, 0, 0, 0, 0, 6, 0]]
+    assert get_all_boxes(question) == answer
+
+def test_is_not_satisfied(wrong_answer):
+    assert is_satisfied(wrong_answer) == False
+
+def test_is_satisfied(answer):
+    assert is_satisfied(answer) == True
+
+def test_guess_is_incorrect(wrong_answer):
+    assert guess_is_correct(wrong_answer) == False
+
+def test_guess_is_correct(wrong_answer):
+    assert guess_is_correct(wrong_answer) == False
+
+def test_find_empty(question):
+    assert find_empty(question) == (0,2)
+
+def test_find_empty_complete(answer):
+    assert find_empty(answer) == None
 
 
-@pytest.fixture
-def solver_with_answer(answer):
-    return SudokuSolver(answer)
 
-
-def test_guess_is_correct(solver_with_answer, answer):
-    solver_with_answer.guess_is_correct(answer)
-    assert solver_with_answer.solution_found == True
-
-
-def test_solver_with_answer(solver_with_answer):
-    solver_with_answer.solve()
-
-
-def test_solver_with_question(solver_with_question):
-    solver_with_question.solve()
-
-
-def test_print_result(solver_with_answer, answer):
-    solver_with_answer.print_result(answer)
-
-
-def test_get_box_coordinates(solver_with_answer, answer):
-    answer = [(0, 6), (0, 3), (0, 0), (3, 6), (3, 3), (3, 0), (6, 6), (6, 3), (6, 0)]
-    assert solver_with_answer.get_box_coordinates() == answer
-
-
-def test_get_boxes(solver_with_answer, answer):
-    answer = [[1, 7, 9, 2, 8, 6, 3, 4, 5],
-              [6, 3, 5, 4, 1, 9, 2, 8, 7],
-              [2, 8, 4, 5, 3, 7, 9, 6, 1],
-              [8, 5, 6, 9, 2, 4, 7, 1, 3],
-              [7, 9, 1, 8, 5, 3, 4, 2, 6],
-              [4, 2, 3, 7, 6, 1, 8, 5, 9],
-              [5, 6, 7, 3, 4, 2, 1, 9, 8],
-              [3, 4, 8, 1, 9, 5, 6, 7, 2],
-              [9, 1, 2, 6, 7, 8, 5, 3, 4]]
-    assert solver_with_answer.get_boxes(answer) == answer
