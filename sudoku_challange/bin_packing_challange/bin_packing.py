4468afd195a92a7a33c4e86b1b9f3d62b24e560d
James Boyle <52674661+J-Boyle@users.noreply.github.com>
2022-01-22 19:54:28 +0000

    started bin packing problem

---
+++ sudoku_challange/bin_packing_challange/bin_packing.py
@@ -0,0 +1,25 @@
+from typing import NamedTuple
+from typing import List
+
+class Service(NamedTuple):
+    MEP: str
+    description: str
+    width: int
+
+
+
+
+def lower_bound():
+    pass
+
+def first_fit():
+    pass
+
+def sort_decreasing():
+    pass
+
+def first_fit_decreasing():
+    pass
+
+def full_bin():
+    pass
